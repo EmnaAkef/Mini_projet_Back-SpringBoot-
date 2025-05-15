4273a5f6942b42c8540717c572ba60fb1d7d3eb5
EmnaAkef <akef.emna@gmail.com>
2025-05-15 04:28:49 +0100

    Ajout des fichiers manquants

---
+++ src/main/java/com/emna/films/security/SecParams.java
@@ -0,0 +1,8 @@
+package com.emna.films.security;
+
+public interface SecParams {
+	public static final long  EXP_TIME = 10*24*60*60*1000;
+	public static final String SECRET = "emna@gmail.com";
+	public static final String PREFIX = "Bearer "; 
+
+}
--- src/main/java/com/emna/films/security/SecurityConfig.java
+++ src/main/java/com/emna/films/security/SecurityConfig.java
@@ -1,59 +1,88 @@
 package com.emna.films.security;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+
+import javax.sql.DataSource;
+import java.util.Arrays;
 
 @Configuration 
+@EnableWebSecurity
 public class SecurityConfig { 
   
-	 @Bean
-	 SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-	             http.authorizeHttpRequests((requests)->requests
-	                		.requestMatchers("/modifierFilm","/supprimerFilm").hasAnyAuthority("ADMIN")
-	                		.requestMatchers("/showCreate","/saveFilm").hasAnyAuthority("ADMIN","AGENT")
-	                		.requestMatchers("/ListeFilms").hasAnyAuthority("ADMIN","AGENT","USER")
-	                		.anyRequest().authenticated())
-                         
-	                .formLogin(Customizer.withDefaults())
-	                .httpBasic(Customizer.withDefaults())
-	                .exceptionHandling((exception)-> exception.accessDeniedPage("/accessDenied"));
-	        return http.build();
-	    }
- 
-     @Bean 
-     public InMemoryUserDetailsManager userDetailsService() { 
-    	 PasswordEncoder passwordEncoder = passwordEncoder ();
-    	 
-      UserDetails admin = User 
-                 .withUsername("admin") 
-                 .password(passwordEncoder.encode("123")) 
-                 .authorities("ADMIN") 
-                 .build(); 
-         UserDetails userEmna = User
-                 .withUsername("emna") 
-                 .password(passwordEncoder.encode("123")) 
-                 .authorities("AGENT","USER") 
-                 .build(); 
-         UserDetails user1 = User
-                 .withUsername("user1") 
-                 .password(passwordEncoder.encode("123")) 
-                 .authorities("USER") 
-                 .build();  
-          
-         return new InMemoryUserDetailsManager(admin, userEmna,user1); 
- 
-     } 
-     @Bean
-	    public PasswordEncoder passwordEncoder () {
-	  	  return new BCryptPasswordEncoder();
-	    }
- 
+    /*@Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults()) // Active la configuration CORS
+            .authorizeHttpRequests((requests)->requests
+                .requestMatchers("/modifierFilm","/supprimerFilm").hasAnyAuthority("ADMIN")
+                .requestMatchers("/showCreate","/saveFilm").hasAnyAuthority("ADMIN","AGENT")
+                .requestMatchers("/ListeFilms").hasAnyAuthority("ADMIN","AGENT","USER")
+                .requestMatchers("/login","/webjars/**","/api/**","/genre/**").permitAll() 
+                .anyRequest().authenticated())
+            .csrf(csrf->csrf.disable())
+            .formLogin((formLogin) -> formLogin 
+                .loginPage("/login") 
+                .defaultSuccessUrl("/"))
+            .httpBasic(Customizer.withDefaults())
+            .exceptionHandling((exception)-> exception.accessDeniedPage("/accessDenied"));
+        return http.build();
+    }
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Autorise Angular
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }*/
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	@Bean 
+	 public SecurityFilterChain filterChain (HttpSecurity http) throws 
+	Exception 
+	 { 
+	  http.sessionManagement( session ->  
+	  session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
+	  .csrf( csrf -> csrf.disable())  
+	            .authorizeHttpRequests( requests -> 
+	requests.requestMatchers("/api/all/**").hasAnyAuthority("ADMINE","USER")
+			.requestMatchers(HttpMethod.GET, "/api/getbyid/**").hasAnyAuthority("ADMINE","USER")
+			.requestMatchers(HttpMethod.POST, "/api/addfilm/**").hasAnyAuthority("ADMINE")
+			.requestMatchers(HttpMethod.PUT, "/api/updatefilm/**").hasAnyAuthority("ADMINE")
+			.requestMatchers(HttpMethod.DELETE, "/api/delfilm/**").hasAnyAuthority("ADMINE")
+	.anyRequest().authenticated() )
+	.addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+	  
+	   
+	 return http.build(); 
+	 } 
+	
 }
